f247205e4087c582bccd71fbd8504cd834f0526c
Junlin Huang <cricketlong@gmail.com>
2020-05-27 01:46:45 +0200

    add support to multi bullets

--- barrage.js
+++ barrage.js
@@ -8,35 +8,55 @@ Component({
   },
 
   data: {
-    left: 200,
-    pace: 2,
-    windowWidth: 200,
+    bullets: [],
+    left: 0,
+    pace: 5,
+    windowWidth: 0,
     length: 20,
-    interval: 500
+    interval: 200
   },
 
   methods: {
     moveBullet: function() {
       var thisPage = this;
       var timer = setInterval(()=> {
-        if(-thisPage.data.left < thisPage.data.length) {
-          thisPage.setData({
-            left: thisPage.data.left - thisPage.data.pace
-          });
-          console.log("move left");
-        } else {
-          clearInterval(timer);
-          thisPage.setData({
-            left: thisPage.data.windowWidth
-          });
-          console.log("reset");
-          thisPage.moveBullet();
+        var bullets = this.data.bullets;
+        for (var i = 0;i < bullets.length;i++) {
+          if(-bullets[i].left < bullets[i].width) {
+            bullets[i].left -= thisPage.data.pace;
+            console.log("move left");
+          } else {
+            bullets[i].left = thisPage.data.windowWidth;
+            console.log("reset");
+          }
         }
+
+        // update all bullets
+        thisPage.setData({
+          bullets: bullets
+        });
+
       }, thisPage.data.interval);
+
+      thisPage.setData({
+        timer: timer
+      });
+    },
+
+    pushBackBullet(text, left, top) {
+      this.data.bullets.push({"text": text, "left": left, "top": top, "width": text.length*10});
     }
   },
 
   ready() {
+    this.setData({
+      left: wx.getSystemInfoSync().windowWidth,
+      windowWidth: wx.getSystemInfoSync().windowWidth
+    });
+
+    this.pushBackBullet("biubiubiu", this.data.windowWidth, 0);
+    this.pushBackBullet("aabbcc", this.data.windowWidth, 50);
+
     this.moveBullet();
   },
 })
