2034d36c0d9b1b1f12f9e2b50eb4d7399f84d1b4
Junlin Huang <cricketlong@gmail.com>
2020-05-28 02:10:34 +0200

    support window in bullet pool

--- barrage.js
+++ barrage.js
@@ -9,6 +9,9 @@ Component({
 
   data: {
     bullets: [],
+    bulletsPool: [],
+    windowSize: 2,
+    nextBulletIndex: -1,
     left: 0,
     pace: 5,
     windowWidth: 0,
@@ -24,10 +27,18 @@ Component({
         for (var i = 0;i < bullets.length;i++) {
           if(-bullets[i].left < bullets[i].width) {
             bullets[i].left -= thisPage.data.pace;
-            console.log("move left");
+            console.log("left");
           } else {
-            bullets[i].left = thisPage.data.windowWidth;
-            console.log("reset");
+            bullets[i].left = this.data.windowWidth;
+            if (thisPage.hasNextBullet()) {
+              bullets[i].displaying = false;
+              bullets[i] = thisPage.getNextBullet();
+              bullets[i].displaying = true;
+              console.log("next");
+            }
+            else {
+              console.log("reset");
+            }
           }
         }
 
@@ -43,9 +54,71 @@ Component({
       });
     },
 
-    pushBackBullet(text, left, top) {
-      this.data.bullets.push({"text": text, "left": left, "top": top, "width": text.length*10});
-    }
+    addBulletToPool(text, left, top) {
+      this.data.bulletsPool.push({"text": text,
+                                  "left": left,
+                                  "top": top,
+                                  "width": text.length*10,
+                                  "displaying": false});
+    },
+
+    /*
+    emplaceFrontBullet(text, left, top) {
+      this.pushFrontBullet({"text": text, "left": left, "top": top, "width": text.length*10});
+    },
+
+    emplaceBackBullet(text, left, top) {
+      this.pushBackBullet({"text": text, "left": left, "top": top, "width": text.length*10});
+    },
+
+    pushFrontBullet(obj) {
+      this.data.bullets.unshift(obj);
+    },
+
+    pushBackBullet(obj) {
+      this.data.bullets.push(obj);
+    },
+
+    popFrontBullet() {
+      this.data.bullets.shift();
+    },*/
+
+    popBackBullet() {
+      this.data.bullets.pop();
+    },
+
+    initBullets() {
+      if (this.data.windowSize > 0) {
+        for (var i = 0;i < this.data.windowSize;i++) {
+          this.data.bullets.push(this.data.bulletsPool[i]);
+          this.data.bullets[i].displaying = true;
+        }
+        this.data.nextBulletIndex = this.data.windowSize;
+      }
+    },
+
+    getNextBullet() {
+      var b = this.data.bulletsPool[this.data.nextBulletIndex++];
+
+      // loop maximally this.data.bulletsPool.length times.
+      for (var n = 0;n < this.data.bulletsPool.length;n++) {
+        if (this.data.nextBulletIndex >= this.data.bulletsPool.length) {
+          this.data.nextBulletIndex = 0;
+        }
+
+        if (this.data.bulletsPool[this.data.nextBulletIndex].displaying == true) {
+          this.data.nextBulletIndex++;
+        }
+      }
+
+      console.log("nextIndex: " + this.data.nextBulletIndex);
+
+      return b;
+    },
+
+    hasNextBullet() {
+      return this.data.nextBulletIndex >= 0;
+    },
   },
 
   ready() {
@@ -54,8 +127,14 @@ Component({
       windowWidth: wx.getSystemInfoSync().windowWidth
     });
 
-    this.pushBackBullet("biubiubiu", this.data.windowWidth, 0);
-    this.pushBackBullet("aabbcc", this.data.windowWidth, 50);
+    // fill pool
+    this.addBulletToPool("biubiubiubiubiubiu", this.data.windowWidth, 0);
+    this.addBulletToPool("a", this.data.windowWidth, 15);
+    this.addBulletToPool("112233", this.data.windowWidth, 30);
+    //this.addBulletToPool("^_^", this.data.windowWidth, 45);
+    //this.addBulletToPool("<=====>", this.data.windowWidth, 60);
+
+    this.initBullets();
 
     this.moveBullet();
   },
